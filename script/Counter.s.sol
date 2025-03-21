6c02e9741d1c0b0ecd3af38b01824e44fb341644
sumana10 <sumanaonlyme@gmail.com>
2025-03-21 22:42:47 +0530

    bridge contract

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {BridgeContract} from "../src/BridgeContract.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    BridgeContract public bridgeContract;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        bridgeContract = new BridgeContract();
 
         vm.stopBroadcast();
     }
