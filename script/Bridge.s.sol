2338a00bff36ee3b7679a5aed9e237b074419faa
sumana10 <sumanaonlyme@gmail.com>
2025-03-23 13:31:25 +0530

    add script

--- script/Bridge.s.sol
+++ script/Bridge.s.sol
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {BridgeContract} from "../src/BridgeContract.sol";
 
-contract CounterScript is Script {
+contract BridgeScript is Script {
     BridgeContract public bridgeContract;
 
     function setUp() public {}
